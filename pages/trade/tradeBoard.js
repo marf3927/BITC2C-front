63e2bc147762d5b61830065c2372461edd966875
marf3927 <marf3927@gmail.com>
2019-10-11 17:15:08 +0900

    기능 : TradeBoard에 getItems fn 추가

--- pages/trade/tradeBoard.js
+++ pages/trade/tradeBoard.js
@@ -1,45 +1,32 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import Link from 'next/link';
 import Head from 'next/head';
 import AppLayout from '../../components/AppLayout';
 import axios from 'axios';
 const tradebox = require('../../module/tradeBox');
 
-
-
-
 const TradeBoard =()=> {
-    const [Item,setItem] = useState({content:[]});
-    let i =1;
+    const [items, setItems] = useState('')
 
-    
+    const baseURL = 'http://localhost:5555'
+    const i = 1
+    function getItems() {
+        axios.get(baseURL +'/tradeboards/index/1')
+            .then((response) => {
+                console.log(response.data)
+                const data = response.data
+                setItems(data)
+            })
+    }
 
-    axios.get('http://localhost:5555/tradeboards/index/'+i).then((response)=>{
-        console.log('yamyam',response.data);
-        setItem(response.data)
-        console.log(Item)
-        return i
-        }).catch((e) =>{
-            console.log(e)
-    });
+    useEffect(() =>{
+        getItems()
+    },[])
 
-   
-        
-    
-    console.log(2);
-    
-   
-    
-    
-    
-    // item.map((userItem)=>{
-    //     const item = userItem;
-    //     console.log(item.id);
-    // })
+    console.log('items :',items)
+
     return (
-        
 
-        
         <AppLayout>
             
             <div className="trade-list-in">
@@ -63,9 +50,11 @@ const TradeBoard =()=> {
                     <span className="price average">10000000krw</span>
                     <div className="operation average">
                         <div className="trade-btn-control">
-                            <button class="ui button">
+                            <button className="ui button">
                                 Follow
                             </button>
+                        </div>
+                        <div>
                         </div>
                     </div>
                 </div>
