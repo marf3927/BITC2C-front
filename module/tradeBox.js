df1f68c745578e919ad5067dfd23424587d2f1df
dongwankim <zkdlwu94@naver.com>
2019-10-11 12:59:10 +0900

    dongwan 2019-10-10 13:00

---
+++ module/tradeBox.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import axios from 'axios';
+
+function getBoarditem(page){
+    let i =page;
+    axios.get('http://localhost:5555/tradeboards/index/'+i).then((response)=>{
+        console.log(response)
+        return  response.data
+    });
+
+    
+}
+
+
+const TradeBox=()=>{
+    return
+
+
+}
+
+module.exports ={getBoarditem}
