49f90cdf2544d889de6b693930707b30e2dab7b6
marf3927 <marf3927@gmail.com>
2019-10-13 19:09:56 +0900

    기능 : Trade Board List View

--- pages/trade/tradeBoard.js
+++ pages/trade/tradeBoard.js
@@ -1,67 +1,93 @@
-import React,{useState, useEffect} from 'react';
-import Link from 'next/link';
-import Head from 'next/head';
-import AppLayout from '../../components/AppLayout';
-import axios from 'axios';
-const tradebox = require('../../module/tradeBox');
+import React, {useState, useEffect} from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
+import AppLayout from '../../components/AppLayout'
+import {Button, Table, Input, Icon} from 'semantic-ui-react'
+import axios from 'axios'
 
-const TradeBoard =()=> {
-    const [items, setItems] = useState('')
+const tradebox = require('../../module/tradeBox')
+
+const TradeBoard = () => {
+    const [items, setItems] = useState([])
+    const [page, setPage] = useState(1)
 
     const baseURL = 'http://localhost:5555'
-    const i = 1
+
+    useEffect(() => {
+        getItems()
+    }, [ ,page])
+
     function getItems() {
-        axios.get(baseURL +'/tradeboards/index/1')
+        axios.get(baseURL + '/tradeboards/index/'+page)
             .then((response) => {
-                console.log(response.data)
                 const data = response.data
                 setItems(data)
             })
     }
 
-    useEffect(() =>{
-        getItems()
-    },[])
+    function NextPageClick() {
+        setPage(page+1)
+    }
 
-    console.log('items :',items)
+    function PreviousPageClick() {
+        if (page>1){
+            setPage(page-1)
+        }
+    }
 
     return (
+        <>
+            <Head>
+                <title>BITC2C</title>
+                <link
+                    rel="stylesheet"
+                    href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.11/semantic.min.css"
+                />
+            </Head>
+            <AppLayout>
+                <Table singleLine>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>method</Table.HeaderCell>
+                            <Table.HeaderCell>status</Table.HeaderCell>
+                            <Table.HeaderCell>type</Table.HeaderCell>
+                            <Table.HeaderCell>price</Table.HeaderCell>
+                            <Table.HeaderCell>amount</Table.HeaderCell>
+                            <Table.HeaderCell>updated</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        {items.map((item) => {
+                            return <Table.Row key={item.id}>
+                                    <Table.Cell>{item.method}</Table.Cell>
+                                    <Table.Cell>{item.status}</Table.Cell>
+                                    <Table.Cell>{item.type}</Table.Cell>
+                                    <Table.Cell>{item.price}</Table.Cell>
+                                    <Table.Cell>{item.amount}</Table.Cell>
+                                    <Table.Cell>{item.updatedAt}</Table.Cell>
+                                </Table.Row>
+                        })}
 
-        <AppLayout>
-            
-            <div className="trade-list-in">
-                <div className="user-list average">
-                    <div className="avatar-container middle zero">
+                    </Table.Body>
+                </Table>
 
-                    </div>
+                <div>
+                    <span><Button onClick={() => PreviousPageClick()}><Icon name="caret left"/></Button></span>
+                    <span><a>{page}</a></span>
+                    <span><Button onClick={() => NextPageClick()}><Icon name="caret right"/></Button></span>
+                    <span className="ui icon input">
+                        <Input type="text" id="page" name="page" placeholder="Page..."/>
+                        <i aria-hidden="true" className="search circular link icon" onClick={e => {
+                            if(document.getElementById('page').value>=1){
+                                setPage(document.getElementById('page').value)
+                            }
+                        }}></i>
+                    </span>
+
                 </div>
+            </AppLayout>
+        </>
+    )
+}
 
-                <div calssName="info-wrapper">
-                    <div className="name width20 spe-width">
-                        <a>
-                            <span>
-                                <span className="font-weight">item</span>
-                                <div className="icon-tips-hover ivu-tooltip"></div>
-                            </span>
-                        </a>
-                    </div>
-                    <span className="stars width20 average spe-width18">0.3729amount</span>
-                    <span className="amout width20 average">1000000krw</span>
-                    <span className="price average">10000000krw</span>
-                    <div className="operation average">
-                        <div className="trade-btn-control">
-                            <button className="ui button">
-                                Follow
-                            </button>
-                        </div>
-                        <div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </AppLayout>
-
-    );
-};
-
-export default TradeBoard;
+export default TradeBoard
