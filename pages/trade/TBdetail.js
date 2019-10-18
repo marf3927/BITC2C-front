b859782b660c1e2083f638280f140031c00249ca
songchip <scv487@naver.com>
2019-10-18 13:48:47 +0900

    tmp

--- pages/trade/TBdetail.js
+++ pages/trade/TBdetail.js
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 import Link from 'next/link'
-import { useSelector } from "react-redux";
-import { useRouter } from 'next/router'
+import { useSelector} from "react-redux";
+import {useRouter} from 'next/router'
 import AppLayout from '../../components/AppLayout'
 import axios from 'axios'
 import fetch from 'isomorphic-unfetch'
-import Router from 'next/router'
 
 
 const TBdetail= ({id})=>{
@@ -13,12 +12,12 @@ const TBdetail= ({id})=>{
     const baseURL = useSelector(state => state.auth.baseURL, [])
 
     const router = useRouter();
-    console.log("3333: ", router);
+    console.log("3333: ", router.query);
     const [items,setItems] =useState([])
 
     //console.log('asdasd',id);
     useEffect(() => {
-
+        
         getItems()
     }, [])
 
@@ -32,39 +31,21 @@ const TBdetail= ({id})=>{
             setItems(data)
 
         })
-      
+        console.log("asd2")
     }
-    // console.log(props.key);
-    function gotoTrade(){
-
-        Router.push('/trade/TBtrade')
-    }
+   // console.log(props.key);
     return (
         <>
 
-
-            <AppLayout>
-                <div className="ui two column centered grid">
-                    {JSON.stringify(items)}
-  
-                        <div className="four column centered row">
-                            <div className="column">
-                                <button className="ui primary button" onClick={()=>gotoTrade()}>
-                                    BUY
-                 </button>
-                            </div>
-                            <div className="column">
-                                
-                            </div>
-                        </div>
-                    </div>
            
+     <AppLayout>
+            
+          {items.type}
 
-               
-                
-            </AppLayout>
 
-        </>
+        </AppLayout>
+
+     </>
     )
 }
 
--- pages/trade/tradeBoard.js
+++ pages/trade/tradeBoard.js
@@ -4,7 +4,7 @@ import AppLayout from '../../components/AppLayout'
 import { useSelector} from "react-redux";
 import {Button, Table, Input, Icon, Tab} from 'semantic-ui-react'
 import axios from 'axios'
-import Router from "next/router"
+
 
 const TradeBoard = () => {
     const [items, setItems] = useState([])
@@ -16,7 +16,9 @@ const TradeBoard = () => {
         getItems()
     }, [, page, selected])
 
-    
+    function getDetail(){
+        return 
+    }
 
     function getItems() {
         if (selected === "All") {
@@ -39,14 +41,7 @@ const TradeBoard = () => {
                 })
         }
     }
-    function gotoDetail(item,status){
-        const itemID=item;
-        const statusCode=status
-        if(status === 0){
-            Router.push('/trade/TBdetail?id='+itemID)
-        }
-        
-    }
+
 
     function tabClick(e) {
         setSelected(e)
@@ -67,22 +62,12 @@ const TradeBoard = () => {
        console.log("writing");
     }
 
-    function statusConfirm(number){
-        console.log(number)
-        if(number ===0){
-            return "Standby"
-        }else if(number === 1){
-            return "Progress"
-        }else {
-            return "completed"
-        }
-    }
     return (
         <>
 
             <AppLayout>
                 <style jsx>{`
-                    .WTbutton {
+                    .type_right {
                         float: right;
                         color: rgb(92, 5, 80);
                     }
@@ -109,15 +94,15 @@ const TradeBoard = () => {
                         <Table.Body>
                             {items.map((item) => {
 
-                                return  <Table.Row key={item.id} onClick={()=>gotoDetail(item.id,item.status)}>
+                                return   <Link href={{ pathname :"/trade/TBdetail", query : {id: item.id}}}  key={item.id} ><Table.Row >
                                     <Table.Cell>{item.method}</Table.Cell>
-                                    <Table.Cell>{statusConfirm(item.status)}</Table.Cell>
+                                    <Table.Cell>{item.status}</Table.Cell>
                                     <Table.Cell>{item.type}</Table.Cell>
                                     <Table.Cell>{item.price}</Table.Cell>
                                     <Table.Cell>{item.amount}</Table.Cell>
                                     <Table.Cell>{item.updatedAt}</Table.Cell>
                                 </Table.Row>
-                                
+                                </Link>
                             })}
                         </Table.Body>
                     </Table>
@@ -135,7 +120,7 @@ const TradeBoard = () => {
                             }
                         }}></i>
                     </span>
-                    <span className="WTbutton"><Button id="WritingBoard" onClick={() => WritingBoard()}>Writing</Button></span>
+                    <span id="type1_right"><Button id="WritingBoard" onClick={() => WritingBoard()}>Writing</Button></span>
                 </div>
             </AppLayout>
        </>
