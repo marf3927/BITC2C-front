11833a095bfa5ab09a1da117a0c26853f7c2aa72
hyosoonim <gytns0087@naver.com>
2019-11-11 15:23:33 +0900

    QnA

--- pages/QnA/detail.js
+++ pages/QnA/detail.js
@@ -1,52 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import Link from 'next/link'
-import AppLayout from '../../components/AppLayout'
-import { Button, Table, Input, Icon, Tab } from 'semantic-ui-react'
-import axios from 'axios'
-import Router from 'next/router'
+import React from 'react';
+import AppLayout from '../../components/AppLayout';
+import { Button, Input } from 'semantic-ui-react'
 
-const Board = () => {
+const Ask = () => {
+    function upload(){
 
-    const ask = () => {
-        Router.push('/QnA/ask');
     }
 
     return (
+        <>
         <AppLayout>
             <div>
-                <h1>Q&A</h1>
-                <Button onClick={ask}>
-                    질문하기
-                </Button>
-                <table border="1">
-                    <tbody>
-                        <tr align="center">
-                            <td width="50">No.</td>
-                            <td width="1100">Title</td>
-                            <td width="120">Name</td>
-                            <td width="150">Date</td>
-                        </tr>
-                        {/* {
-                            boards.map(row =>
-                                (<BoardItem key={row.brdno} row={row} />)
-                            )
-                        } */}
-                    </tbody>
-                </table>
+                <h2>질문 상세보기</h2>
+                <div>
+                    <br />
+                    <br />
+                </div>
+                <div>
+                    제목<Input type="text" placeholder="해당 질문의 제목"/>
+                    <br />
+                    <br />
+                    내용
+                    <br />
+                    <textarea placeholder="해당 질문의 내용" rows="25" cols="230"></textarea>
+                    <Button>
+                        답변하기
+                    </Button>
+                </div>
             </div>
         </AppLayout>
+        </>
     )
 }
 
-const BoardItem = () => {
-    return (
-        <tr>
-            <td>{this.props.row.brdno}</td>
-            <td>{this.props.row.brdtitle}</td>
-            <td>{this.props.row.brdwriter}</td>
-            <td>{this.props.row.brddate.toLocaleDateString('ko-KR')}</td>
-        </tr>
-    )
-}
-
-export default Board;
+export default Ask
